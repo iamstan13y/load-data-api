4dfb16c580ac77edbea63ef696625ff8a5c2b2da
= <=>
2021-07-15 22:43:14 +0200

    first commit.

---
+++ LoadData/Dataload.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data.SQLite
+Imports MySql.Data.MySqlClient
+Imports System.Windows.Forms
+Public Class Dataload
+
+    ' Dim conn As New SQLiteConnection("Data Source =./Rtg.bin")
+    'Dim tablename As String = ""
+
+    Public Sub SQLiteLoad(ByVal tablename As String, ByVal conn As SQLiteConnection, ByVal datagrid As DataGridView)
+        Dim cmd As New SQLiteCommand("SELECT * FROM `" & tablename & "`", conn)
+        Dim adapter As New SQLiteDataAdapter(cmd)
+        Dim dset As New DataSet
+        adapter.Fill(dset)
+        datagrid.DataSource = dset.Tables(0)
+    End Sub
+
+    Public Sub MySqlLoad(ByVal tablename As String, ByVal conn As MySqlConnection, ByVal datagrid As DataGridView)
+
+
+        [Dim cmd As New MySqlCommand("SELECT * FROM `" & tablename & "`", conn)
+        Dim adapter As New MySqlDataAdapter(cmd)
+        Dim dset As New DataSet
+        adapter.Fill(dset)
+        datagrid.DataSource = dset.Tables(0)
+    End Sub
+End Class
